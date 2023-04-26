8a0ae6d707ce868355b393796ccb35847c12ad89
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2023-04-25 23:45:58 -0400

    refactor

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from sublinear.frequency.cms.experiments.experiment1 import test_cms_1
-from sublinear.frequency.cms.experiments.experiment2 import test_cms_2
+from sublinear.frequency_table.cms.experiments.experiment1 import test_cms_1
+from sublinear.frequency_table.cms.experiments.experiment2 import test_cms_2
 from sublinear.distinct_elements.bjkst.experiments.experiment1 import test_bjkst
-from sublinear.frequency.mgs.experiments.experiment1 import test_mgs
+from sublinear.heavy_hitters.mgs.experiments.experiment1 import test_mgs
 
 if __name__ == "__main__":
-    test_bjkst()
+    # test_bjkst()
     # test_cms_1()
-    # test_mgs()
+    test_mgs()
